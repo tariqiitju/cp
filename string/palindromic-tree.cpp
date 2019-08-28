8d7ec1ac1ffd7a884e6ebbc3f6fa51bc28b5c01a
Tariqul Islam <tarik.amtoly@gmail.com>
2019-08-28 23:04:31 +0600

    palindromic tree added

---
+++ string/palindromic-tree.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MX = 1e5 + 5;
+const int _Y = 26;
+#define VAL(c)  ((c)-'a')
+struct node{
+    int len,link,e[_Y];
+    node(){len=link=0;memset(e,0,sizeof(e));}
+    inline void clear(){len=link=0;memset(e,0,sizeof(e));}
+}ALL[MX];
+int avl = 1;                        //Each test case
+inline int New(){
+    ALL[avl].clear();
+    return avl++;
+}
+struct palindromicTree{
+    int rootEven,rootOdd;
+    string s;
+    palindromicTree(){rootEven=rootOdd=0;s="";}
+    palindromicTree(string _s){
+        s = _s;
+        rootOdd = New();
+        ALL[rootOdd].len = - 1;
+        ALL[rootOdd].link = rootOdd;
+        rootEven = New();
+        ALL[rootEven].len = 0;
+        ALL[rootEven].link = rootOdd;
+        int last = rootEven;
+        for(int i=0;s[i];i++){
+            while(s[i - ALL[last].len - 1] != s[i])last = ALL[last].link;
+            int x = ALL[last].link ,c = VAL(s[i]);
+            while(s[i - ALL[x].len - 1] != s[i])x = ALL[x].link;
+            if(!ALL[last].e[c]){
+                int cur = New();
+                ALL[last].e[c] = cur;
+                ALL[cur].len = ALL[last].len + 2;
+                ALL[cur].link = ALL[cur].len == 1 ? rootEven : ALL[x].e[c];
+            }
+            last = ALL[last].e[c];
+        }
+    }
+};
